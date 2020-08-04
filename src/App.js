1356fd0c82cd5a2283ea36d5e8fb567435c135fb
Jamie Sear <jamiesear@jamies-macbook.home>
2020-08-04 12:02:54 +0100

    Added babel plugin for component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
+import React, { Component } from 'react';
+// import ReactDOM from 'react-dom'
 import './App.css'
 
-class App extends React.Component {
-    render() {
-      const favArtists = 'Our favourite artists: '
-        const artists = [
+class App extends Component {
+    state = {
+        favArtists: 'Our favourite artists: ',
+        likeCount: 0,
+        artists: [
             {
                 id: 0,
                 name:'A$AP Rocky',
@@ -27,8 +28,12 @@ class App extends React.Component {
                 img: 'https://media.pitchfork.com/photos/5929b3995e6ef9596932249e/1:1/w_320/1192269b.jpg',
                 intro: 'Damn is the fourth studio album by American rapper Kendrick Lamar. It was released on April 14, 2017, through Top Dawg Entertainment, distributed by Aftermath Entertainment and Interscope Records.'
             }
-            ]
+        ]
+    }
 
+    increaseLikesCount = () => this.setState(prevState => ({ likeCount: prevState.likeCount++}));
+
+    render() {
         return (
             <main>
               <div>
@@ -39,6 +44,7 @@ class App extends React.Component {
                     <div className="container">
                         <h4>{artists[0].name}</h4>
                       <p>{artists[0].intro}</p>
+                      <button onClick ></button>
                     </div>
                 </div>
 
